4cf7d547e06535b609b5c2a5c8bdc9b1d380ade3
inimitableharish <inimitableharish@gmail.com>
2018-04-05 18:12:25 +0530

    retry after some time

--- retry_logic.py
+++ retry_logic.py
@@ -1,6 +1,7 @@
 
 
 MAX_RETRIES = 10
+import time 
 
 def some_fnc():
     retry_number = 1
@@ -13,6 +14,8 @@ def some_fnc():
             x = 1/0
         except Exception as e:
             print('Retrying number ' +str(retry_number))
+            # Wait for 30 sec before retry 
+            time.sleep(30)
             continue 
         break
 
