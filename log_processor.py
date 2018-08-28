77f8de356ce0a44708b04f80934db529a6efedcb
inimitableharish <inimitableharish@gmail.com>
2018-08-28 18:52:12 +0530

    pass and retrieve all the log lines as list

--- log_processor.py
+++ log_processor.py
@@ -1,7 +1,17 @@
+#!/usr/bin/python
+
 import sys
 
-error_log = list(sys.argv[1])
-for element in error_log:
-	print(element)
 
+print('-'*100)
+
+error_log = sys.argv[1:]
+
+
+#for element in error_log:
+#	print(element)
+print('*'*100)
+print(error_log)
+print('+'*100)
+print(type(error_log))
 print('hello worldddddd')
