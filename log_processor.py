29ece0aba0caeaadd372a1c669b1a8ef90593f76
inimitableharish <inimitableharish@gmail.com>
2018-08-28 18:58:46 +0530

    minor error skip logic

--- log_processor.py
+++ log_processor.py
@@ -8,10 +8,19 @@ print('-'*100)
 error_log = sys.argv[1:]
 
 
+ignore_list = ['wifi error', 'preferred network']
+
 #for element in error_log:
 #	print(element)
 print('*'*100)
-print(error_log)
-print('+'*100)
-print(type(error_log))
-print('hello worldddddd')
+for log_line in error_log:
+	for skip_error in ignore_list:
+		if skip_error in log_line:
+			continue
+		else:
+			print('\n')
+			print('Fatal error found ->', log_line)
+			print('.'*100)
+
+
+
