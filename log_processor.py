0418e06cfc002bf8c0e2a0bd62a9b8ea64c85c23
inimitableharish <inimitableharish@gmail.com>
2018-08-28 19:02:36 +0530

    added fatal and minor error

--- log_processor.py
+++ log_processor.py
@@ -9,18 +9,19 @@ error_log = sys.argv[1:]
 
 
 ignore_list = ['wifi error', 'preferred network']
+fatal_error = ['quark-agent','redis connectivity']
 
-#for element in error_log:
-#	print(element)
-print('*'*100)
 for log_line in error_log:
-	for skip_error in ignore_list:
-		if skip_error in log_line:
-			continue
-		else:
+	for major_error in fatal_error:
+		if major_error in log_line:
 			print('\n')
 			print('Fatal error found ->', log_line)
 			print('.'*100)
+	for minor_error in ignore_list:
+		if minor_error in log_line:
+			print('\n')
+			print('Minor error found skipping')
+			
 
 
 
